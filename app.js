6b36f451ac401453eac3a90a501b379aeb9953b1
Sara Masek <smasek09@gmail.com>
2021-10-02 21:43:17 -0400

    Overhauling some of the design, including new background for the recipe card containers, a slimmer main, and I took away the polaroid-photo look the cards previously had. Switching color scheme to a black and white look - beginning on responsive mobile design.

---
+++ app.js
@@ -0,0 +1,24 @@
+//access the heart icon
+const heartIcon = document.querySelector(".svg-icon");
+const recipeCard = document.querySelector(".rcp-card");
+//access the recipe card
+// const recipe = document.querySelector(".rcp-card");
+const recipes = [];
+//when the heart icon is clicked, add to an array of liked recipes
+//Add recipes to a separate part of the page - in the header?
+
+// heartIcon.addEventListener("click", (e) => {
+//     const recipe = document.querySelector(".rcp-card");
+//     if (e.target === heartIcon) {
+//         //add this recipe to the recipe list
+//         recipes.push(recipe);
+//         console.log("recipe added");
+
+//     }
+// });
+
+// recipeCard.addEventListener('click', (e){
+//     //create a new div card that pops out
+//     //add a new class to that card to apply the styles
+// });
+
